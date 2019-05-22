cb74a72989c9dbb55ca220e5909be979a4e986e1
Mark-UdacityStudent <Mark.L.Orzechowski@gmail.com>
2019-05-22 04:37:17 -0700

    Add Feature2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,13 +9,16 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {          
+        {
             // Code was added in GitHub.
             // Code was edited in GitHub.
 
             // Code was added in VS.
 
             // Code to call Feature1
+
+            // Code to call Feature2
+
         }
     }
 }
